fcdcace13971147654f24e9237b141b0fa42d5f3
Thomas Noell <58270020+OfficialThomas@users.noreply.github.com>
2023-04-12 16:43:51 -0700

    playable demo scene

--- Project/Assets/Scripts/Characters/Player.gd
+++ Project/Assets/Scripts/Characters/Player.gd
@@ -1,28 +1,42 @@
 extends CharacterBody2D
 
+# Animation and audio
+@onready var sprite = $AnimatedSprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+# Jumping and moving
+const  SPEED = 400.0
+const JUMP_VELOCITY = -1150
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var bounce = 0
+var b_multi = 1.25
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
+	# Gravity and Auto Jump
+	if bounce == 1:
+		velocity.y = JUMP_VELOCITY * b_multi
+		bounce = 0
+	elif not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	else:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+#	Directional animation state
+	if Input.is_action_pressed("left"):
+		sprite.play("left")
+	elif Input.is_action_pressed("right"):
+		sprite.play("right")
+	else:
+		sprite.play("foward")
 
 	move_and_slide()
